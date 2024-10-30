42f286e76a6151df441c3c82871bef07d8e7a158
Juan Pablo Cruz Sanchez <juanp.cruz.sanchez@gmail.com>
2024-10-30 07:14:54 -0600

    Cambios

--- gestiGastillos/src/main/java/com/example/gestiGastillos/dto/card/CardDataDTO.java
+++ gestiGastillos/src/main/java/com/example/gestiGastillos/dto/card/CardDataDTO.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.NotBlank;
 public record CardDataDTO(
         @NotBlank(message = "'name' no puede estar vacio")
         String name,
+
         @NotBlank(message = "'last_digits' no puede estar vacio")
         @JsonProperty("last_digits")
         String lastDigits,
+
         @NotBlank(message = "'expiration_date' no puede estar vacio")
         @JsonProperty("expiration_date")
         String expirationDate
