7b4288fee9c9a4409245d6b247cf6687469029a6
JuanComparan <valeriocomparanj.@gmail.com>
2024-11-23 11:40:48 -0600

    DoubleBarChart

--- Front-End/gestiGastillos/components/BarChartComponent.tsx
+++ Front-End/gestiGastillos/components/BarChartComponent.tsx
@@ -1,50 +1,68 @@
 import React from "react";
-import { Text, Dimensions, StyleSheet } from "react-native";
+import { Text, Dimensions, StyleSheet, View } from "react-native";
 import globalStyles from "@/styles/GlobalStyles";
-import BarChart from "react-native-chart-kit/dist/BarChart";
+import { BarChart } from "react-native-gifted-charts";
 
 export default function BarChartComponent() {
-    const screenWidth = Dimensions.get('window').width; // Obtener el ancho de la pantalla
+
+    const data = [
+        { value: 2500, label: 'Enero', frontColor: '#0ED418FF' },
+        { value: 2400, frontColor: '#F11818FF' },
+        { value: 3500, label: 'Febrero', frontColor: '#0ED418FF' },
+        { value: 3000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Marzo', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Abril', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Mayo', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Junio', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Julio', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Agosto', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Septiembre', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Octubre', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Noviembre', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+        { value: 4500, label: 'Diciembre', frontColor: '#0ED418FF' },
+        { value: 4000, frontColor: '#F11818FF' },
+    ];
 
     return (
-        <>
-            <Text style={globalStyles.title}>Tu balance</Text>
-            <BarChart
-                data={{
-                    labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun'],
-                    datasets: [
-                        {
-                            data: [10000, 13000, 12000, 10500, 11000, 10000],
-                        },
-                        {
-                            data: [300, 800, 1500, 5000, 1500, 2000],
-                        },
-                    ],
-                }}
-                width={screenWidth - 20}
-                height={250}
-                yAxisLabel="$"
-                yAxisSuffix=""
-                chartConfig={{
-                    backgroundGradientFrom: '#eff3ff',
-                    backgroundGradientTo: '#efefef',
-                    decimalPlaces: 0,
-                    barPercentage: 0.5,
-                    color: (opacity = 1) => `rgba(39, 193, 249, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                }}
-                style={styles.graphContainer}
-                fromZero
-                verticalLabelRotation={20}
-            />
-        </>
+            <View style={styles.graphContainer}>
+                <Text style={globalStyles.title}>Tu balance</Text>
+                <View style={{ padding: 20, alignItems: 'center'}}>
+                    <BarChart
+                        data={data}
+                        barWidth={16}
+                        initialSpacing={10}
+                        spacing={14}
+                        barBorderRadius={4}
+                        yAxisThickness={0}
+                        xAxisType={'dashed'}
+                        xAxisColor={'#27C1F9'}
+                        yAxisTextStyle={{ color: '#4f4f4f' }}
+                        stepValue={1000}
+                        maxValue={6000}
+                        noOfSections={6}
+                        yAxisLabelTexts={['0', '1k', '2k', '3k', '4k', '5k', '6k']}
+                        labelWidth={40}
+                        xAxisLabelTextStyle={{ color: '#4f4f4f', textAlign: 'center' }}
+                    />
+                </View>
+            </View>
     );
 }
 
 const styles = StyleSheet.create({
     graphContainer: {
-        flex: 0.5,
-        marginRight: 10,
+        width: '95%',
         marginVertical: 20,
+        elevation: 3,
+        backgroundColor: '#f4f4f4'
     },
 })
